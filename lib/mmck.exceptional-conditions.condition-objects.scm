4426b69a3f75789e355c3c03d832a2110db11587
Marco Maggi <marco.maggi-ipsu@poste.it>
2019-07-15 15:27:33 +0200

    development of condition objects

--- lib/mmck.exceptional-conditions.condition-objects.scm
+++ lib/mmck.exceptional-conditions.condition-objects.scm
@@ -27,17 +27,493 @@
 ;;;; units and module header
 
 (declare (unit mmck.exceptional-conditions.condition-objects)
+	 (uses mmck.exceptional-conditions.helpers)
 	 (emit-import-library mmck.exceptional-conditions.condition-objects))
 
 (module (mmck.exceptional-conditions.condition-objects)
-    (the-func)
-  (import (scheme))
+    ((syntax: define-condition-type
+	      chicken::make-composite-condition
+	      chicken::make-property-condition
+	      chicken::condition-predicate
+	      chicken::condition-property-accessor
+	      make-<condition>
+	      <condition>-constructor
+	      <condition>-total-number-of-fields
+	      split-constructor-args
+	      zip-constructor-args)
+     ;; the following are defined by R6RS
+     &condition make-condition condition?
+     &who make-who-condition who-condition? condition-who
+     &message make-message-condition message-condition? condition-message
+     &irritants make-irritants-condition make-irritants-condition/from-args irritants-condition? condition-irritants
+     &warning make-warning warning?
+     &serious make-serious-condition serious-condition?
+     &error make-error error?
+     &violation &serious make-violation violation?
+     &assertion &serious make-assertion-violation assertion-violation?
+     &non-continuable make-non-continuable-violation non-continuable-violation?
+     &implementation-restriction make-implementation-restriction-violation implementation-restriction-violation?
+     &lexical make-lexical-violation lexical-violation?
+     &syntax make-syntax-violation syntax-violation?
+     &undefined make-undefined-violation undefined-violation?
+     ;;
+     error
+     assertion-violation
+     raise
+     raise-continuable
+     condition
+     condition?
+     simple-conditions)
+  (import (scheme)
+	  (prefix (only (chicken base)
+			error
+			define-record-printer)
+		  chicken::)
+	  (prefix (chicken condition)
+		  chicken::)
+	  (only (chicken format)
+		format)
+	  (prefix (only (chicken condition)
+			current-exception-handler
+			print-error-message
+			condition?)
+		  chicken::)
+	  (mmck exceptional-conditions helpers))
+  (import-for-syntax (scheme)
+		     (only (chicken base)
+			   define-record)
+		     (only (chicken syntax)
+			   er-macro-transformer)
+		     (only (matchable)
+			   match)
+		     (mmck exceptional-conditions helpers))
 
 
-;;;; miscellaneous functions
+;;;; helpers
 
-(define (the-func)
-  #t)
+(define-syntax %internal-assert
+  (syntax-rules ()
+    ((_ ?expr)
+     (unless ?expr
+       (chicken::error 'assert "failed assertion" (quote ?expr))))))
+
+
+;;;; exceptional-condition objects API
+;;
+;;This is meant to be functionally equivalent to the API defined by "(rnrs conditions (6))".
+;;
+
+(define-record <condition>
+  name
+		;A symbol representing the condition kind name.
+  parent
+		;The  parent "<condition>"  object.  Set  to #f  if this  type has  no parent;  only
+		;"&condition" has no parent.
+  constructor
+		;The exceptional-condition object-type constructor procedure.
+  predicate
+		;The exceptional-condition object-type predicate procedure.
+  number-of-fields
+		;A non-negative fixnum representing the number of fields for this type.
+  total-number-of-fields
+		;A non-negative fixnum representing  the number of fields for this  type and all its
+		;subtypes.
+  #| end of DEFINE-RECORD |# )
+
+(define (condition . cnds)
+  (if (null? cnds)
+      (make-condition)
+    (apply chicken::make-composite-condition cnds)))
+
+(define condition?
+  chicken::condition?)
+
+(define (simple-conditions cnd)
+  (map (lambda (C)
+	 (apply chicken::make-property-condition C))
+    (chicken::condition->list cnd)))
+
+;;; --------------------------------------------------------------------
+
+(define (make-condition)
+  (chicken::make-property-condition '&condition))
+
+(define &condition
+  (make-<condition> '&condition #f make-condition condition? 0 0))
+
+
+;;;; implementation of DEFINE-CONDITION-TYPE: helpers
+
+(define (split-constructor-args type-spec parent-spec args)
+  (let* ((parent-num	(<condition>-total-number-of-fields parent-spec))
+	 (total-num	(<condition>-total-number-of-fields type-spec)))
+    (unless (= total-num (length args))
+      (chicken::abort
+       (chicken::make-property-condition '&mmck-exceptional-conditions-wrong-constructor-arguments
+					 'location (<condition>-name type-spec)
+					 'message (format #f "wrong number of arguments to condition object constructor, \
+                                                              expected ~a, got ~a"
+							  total-num (length args))
+					 'irritants args)))
+    (let loop ((args		args)
+	       (i			0)
+	       (rev-parent-args	'())
+	       (rev-type-args	'()))
+      (cond ((null? args)
+	     (values (reverse rev-parent-args) (reverse rev-type-args)))
+	    ((< i parent-num)
+	     (loop (cdr args) (+ 1 i) (cons (car args) rev-parent-args) rev-type-args))
+	    (else
+	     (loop (cdr args) (+ 1 i) rev-parent-args (cons (car args) rev-type-args)))))))
+
+(define (zip-constructor-args field-names field-values)
+  (if (null? field-names)
+      '()
+    (cons* (car field-names) (car field-values)
+	   (zip-constructor-args (cdr field-names) (cdr field-values)))))
+
+
+;;;; implementation of DEFINE-CONDITION-TYPE
+
+(define-syntax define-condition-type
+  (er-macro-transformer
+    (lambda (input-form.stx rename compare)
+
+
+;;;; syntax DEFINE-CONDITION-TYPE: helpers
+
+(define (synner message . args)
+  (apply syntax-error 'define-condition-type message input-form.stx args))
+
+(define (synner-warning message . args)
+  (apply warning message input-form.stx args))
+
+(define (syntactic-identifier? stx)
+  (symbol? stx))
+
+(define (check-condition-type-name name.stx)
+  (unless (syntactic-identifier? name.stx)
+    (synner "expected syntactic identifier as exceptional-condition object-type name" name.stx)))
+
+(define (check-condition-parent-type-name name.stx)
+  (unless (syntactic-identifier? name.stx)
+    (synner "expected syntactic identifier as exceptional-condition object-type parent name" name.stx)))
+
+(define (check-condition-constructor-name name.stx)
+  (unless (syntactic-identifier? name.stx)
+    (synner "expected syntactic identifier as exceptional-condition object-type constructor name" name.stx)))
+
+(define (check-condition-predicate-name name.stx)
+  (unless (syntactic-identifier? name.stx)
+    (synner "expected syntactic identifier as exceptional-condition object-type predicate name" name.stx)))
+
+(define (check-condition-field-name name.stx)
+  (unless (syntactic-identifier? name.stx)
+    (synner "expected syntactic identifier as exceptional-condition object-type field name" name.stx)))
+
+(define (check-condition-field-accessor-name name.stx)
+  (unless (syntactic-identifier? name.stx)
+    (synner "expected syntactic identifier as exceptional-condition object-type field accessor name" name.stx)))
+
+(define (check-duplicate ell syn)
+  (unless (null? ell)
+    (let ((head (car ell))
+	  (tail (cdr ell)))
+      (if (exists (lambda (item)
+		    (eq? item head))
+	    tail)
+	  (syn head)
+	(check-duplicate tail syn)))))
+
+(define (check-duplicate-field-name field-names)
+  (check-duplicate field-names
+		   (lambda (field-name.id)
+		     (synner "duplicate field name in exceptional-condition object-type definition" field-name.id))))
+
+(define (check-duplicate-field-accessor-name field-accessor-names)
+  (check-duplicate field-accessor-names
+		   (lambda (field-accessor.id)
+		     (synner "duplicate field accessor name in exceptional-condition object-type definition" field-accessor.id))))
+
+
+;;;; syntax DEFINE-CONDITION-TYPE: syntactic identifiers used in the output form
+
+(define %begin		(rename 'begin))
+(define %quote		(rename 'quote))
+(define %define		(rename 'define))
+(define %apply		(rename 'apply))
+(define %receive	(rename 'receive))
+(define %+		(rename '+))
+
+(define %chicken::make-composite-condition	(rename 'chicken::make-composite-condition))
+(define %chicken::make-property-condition	(rename 'chicken::make-property-condition))
+(define %chicken::condition-predicate		(rename 'chicken::condition-predicate))
+(define %chicken::condition-property-accessor	(rename 'chicken::condition-property-accessor))
+
+(define %make-<condition>			(rename 'make-<condition>))
+(define %<condition>-constructor		(rename '<condition>-constructor))
+(define %<condition>-total-number-of-fields	(rename '<condition>-total-number-of-fields))
+
+(define %split-constructor-args			(rename 'split-constructor-args))
+(define %zip-constructor-args			(rename 'zip-constructor-args))
+
+
+;;;; syntax DEFINE-CONDITION-TYPE: data types
+
+(define-record <parsed-input>
+  type-id
+		;A syntactic identifier representing the exceptional-condition object-type name.
+  parent-id
+		;A syntactic  identifier representing  the exceptional-condition  object-type parent
+		;name.
+  constructor-id
+		;A   syntactic  identifier   representing   the  exceptional-condition   object-type
+		;constructor name.
+  predicate-id
+		;A   syntactic  identifier   representing   the  exceptional-condition   object-type
+		;predicate name.
+  fields
+		;A (possibly empty) list of "<field>" records representing the field specifications.
+  #| end of DEFINE-RECORD |# )
+
+(define-record <field>
+  name
+		;The symbol name of a field.
+  accessor
+		;The symbol name of a field's accessor.
+  #| end of DEFINE-RECORD |# )
+
+
+;;;; syntax DEFINE-CONDITION-TYPE: parsing and building
+
+(define (main input-form.stx)
+  (let ((parsed-input (parse-input-form input-form.stx)))
+    (build-output-form parsed-input)))
+
+(define (parse-input-form input-form.stx)
+  (match input-form.stx
+    ((_ ?condition-type-name ?parent-condition-type-name ?constructor-id ?predicate-id (?field-name* ?field-accessor*) ...)
+     (begin
+       (check-condition-type-name		?condition-type-name)
+       (check-condition-parent-type-name	?parent-condition-type-name)
+       (check-condition-constructor-name	?constructor-id)
+       (check-condition-predicate-name		?predicate-id)
+       (for-each check-condition-field-name		?field-name*)
+       (for-each check-condition-field-accessor-name	?field-accessor*)
+       (check-duplicate-field-name		?field-name*)
+       (check-duplicate-field-accessor-name	?field-accessor*)
+       (make-<parsed-input> ?condition-type-name ?parent-condition-type-name ?constructor-id ?predicate-id
+			    (map (lambda (field-name.id field-accessor.id)
+				   (make-<field> field-name.id field-accessor.id))
+			      ?field-name* ?field-accessor*))))
+    (_
+     (synner "syntax error in exceptional-condition object-type definition form"))))
+
+(define (build-output-form parsed-input)
+  (let ((TYPE-ID		(<parsed-input>-type-id		parsed-input))
+	(PARENT-ID		(<parsed-input>-parent-id	parsed-input))
+	(CONSTRUCTOR-ID		(<parsed-input>-constructor-id	parsed-input))
+	(PREDICATE-ID		(<parsed-input>-predicate-id	parsed-input))
+	(FIELD-NAME*		(map <field>-name (<parsed-input>-fields parsed-input))))
+    (let ((NUMBER-OF-FIELDS	(length (<parsed-input>-fields parsed-input)))
+	  (FIELD-NAMES		(map <field>-name (<parsed-input>-fields parsed-input)))
+	  (FIELD-ACCESSORS	(map (lambda (field-spec)
+				       (let ((FIELD-NAME	(<field>-name     field-spec))
+					     (FIELD-ACCESSOR	(<field>-accessor field-spec)))
+					 `(,%define ,FIELD-ACCESSOR
+						    (,%chicken::condition-property-accessor (,%quote ,TYPE-ID) (,%quote ,FIELD-NAME)))))
+				  (<parsed-input>-fields parsed-input))))
+      `(,%begin
+	(,%define (,CONSTRUCTOR-ID . args)
+		  (,%receive (parent-args type-args)
+			     (,%split-constructor-args ,TYPE-ID ,PARENT-ID args)
+			     (,%chicken::make-composite-condition
+			      (,%apply ,%chicken::make-property-condition (,%quote ,TYPE-ID)
+				       (,%zip-constructor-args (,%quote ,FIELD-NAMES)
+							       type-args))
+			      (,%apply (,%<condition>-constructor ,PARENT-ID) parent-args))))
+
+	(,%define ,PREDICATE-ID
+		  (,%chicken::condition-predicate (,%quote ,TYPE-ID)))
+
+	,@FIELD-ACCESSORS
+
+	(,%define ,TYPE-ID
+		  (,%make-<condition> (,%quote ,TYPE-ID) ,PARENT-ID ,CONSTRUCTOR-ID ,PREDICATE-ID
+				      ,NUMBER-OF-FIELDS
+				      (,%+ ,NUMBER-OF-FIELDS (,%<condition>-total-number-of-fields ,PARENT-ID))))
+
+	#| end of %begin |# ))))
+
+
+;;;; syntax DEFINE-CONDITION-TYPE: let's go
+
+(main input-form.stx))))
+
+
+;;;; exceptional-condition object-types defined by R6RS
+
+(define-condition-type &who
+    &condition
+  $make-who-condition
+  who-condition?
+  (who	condition-who))
+
+(define-condition-type &message
+    &condition
+  $make-message-condition
+  message-condition?
+  (message	condition-message))
+
+(define-condition-type &irritants
+    &condition
+  make-irritants-condition
+  irritants-condition?
+  (irritants	condition-irritants))
+
+(define-condition-type &warning
+    &condition
+  make-warning
+  warning?)
+
+(define-condition-type &serious
+    &condition
+  make-serious-condition
+  serious-condition?)
+
+(define-condition-type &error
+    &serious
+  make-error
+  error?)
+
+(define-condition-type &violation
+    &serious
+  make-violation
+  violation?)
+
+(define-condition-type &assertion
+    &serious
+  make-assertion-violation
+  assertion-violation?)
+
+(define-condition-type &non-continuable
+    &serious
+  make-non-continuable-violation
+  non-continuable-violation?)
+
+(define-condition-type &implementation-restriction
+    &serious
+  make-implementation-restriction-violation
+  implementation-restriction-violation?)
+
+(define-condition-type &lexical
+    &serious
+  make-lexical-violation
+  lexical-violation?)
+
+(define-condition-type &syntax
+    &serious
+  make-syntax-violation
+  syntax-violation?)
+
+(define-condition-type &undefined
+    &serious
+  make-undefined-violation
+  undefined-violation?)
+
+
+;;;; raising special exceptions
+
+(define (who-condition-value? obj)
+  (or (symbol? obj)
+      (string? obj)
+      (not     obj)))
+
+(define (message-condition-value? obj)
+  (string obj))
+
+(define (error who message . irritants)
+  ;;R6RS states that there must be an "&irritants" condition.
+  ;;
+  (%internal-assert (who-condition-value?     who))
+  (%internal-assert (message-condition-value? message))
+  (raise
+   (condition
+     (make-error)
+     ($make-who-condition who)
+     ($make-message-condition message)
+     (make-irritants-condition irritants))))
+
+(define (assertion-violation who message . irritants)
+  ;;R6RS states that there must be an "&irritants" condition.
+  ;;
+  (%internal-assert (who-condition-value?     who))
+  (%internal-assert (message-condition-value? message))
+  (raise
+   (condition
+     (make-assertion-violation)
+     ($make-who-condition who)
+     ($make-message-condition message)
+     (make-irritants-condition irritants))))
+
+(define-syntax assert
+  (syntax-rules ()
+    ((_ ?expr)
+     (unless ?expr
+       (error 'assert "failed assertion" (quote ?expr))))))
+
+;;; --------------------------------------------------------------------
+
+(define-syntax %expand-assert*
+  (ir-macro-transformer
+    (lambda (input-form inject compare)
+      (let ((?expr (cadr input-form)))
+	(let ((%__who__ (inject '__who__)))
+	  `(unless ,?expr
+	     (error ,%__who__ "failed assertion" (quote ,?expr))))))))
+
+(define-syntax assert*
+  (syntax-rules ()
+    ((_ ?expr)
+     (%expand-assert* ?expr))
+    ))
+
+
+;;;; extended exceptional-condition object constructors
+
+(define (make-who-condition who)
+  (%internal-assert (who-condition-value? who))
+  ($make-who-condition who))
+
+(define (make-message-condition message)
+  (%internal-assert (string? message))
+  ($make-message-condition message))
+
+(define (make-irritants-condition/from-args . args)
+  (make-irritants-condition args))
+
+
+;;;; raising general exceptions
+
+(define (raise obj)
+  (chicken::abort obj))
+
+(define (raise-continuable obj)
+  (chicken::signal obj))
+
+(define (assertion-violation who message . irritants)
+  (raise
+   (condition (make-assertion-violation)
+	      (make-who-condition who)
+	      (make-message-condition message)
+	      (make-irritants-condition irritants))))
+
+(define (error who message . irritants)
+  (raise
+   (condition (make-error)
+	      (make-who-condition who)
+	      (make-message-condition message)
+	      (make-irritants-condition irritants))))
 
 
 ;;;; done
--- tests/test-condition-objects.scm
+++ tests/test-condition-objects.scm
@@ -39,13 +39,457 @@
 (check-display "*** testing exceptional condition objects\n")
 
 
-(parameterize ((check-test-name		'alpha))
+;;;; helpers
+
+(define (debug-print . args)
+  (import (chicken base)
+	  (chicken pretty-print))
+  (parameterize ((pretty-print-width 150))
+    (pretty-print args (current-error-port))))
+
+
+(parameterize ((check-test-name		'&condition))
 
   (check
-      (the-func)
-    => #t)
+      (let ((C	(make-condition)))
+	(values (condition?         C)
+		(message-condition? C)))
+    => #t #f)
 
-  (values))
+  (check
+      (let ((C	(condition (make-condition)
+			   (make-condition))))
+	(values (condition?         C)
+		(message-condition? C)))
+    => #t #f)
+
+  #f)
+
+
+(parameterize ((check-test-name		'&who))
+
+  (check
+      (let ((C	(make-who-condition 'ciao)))
+	(values (condition?         C)
+		(who-condition?     C)
+		(message-condition? C)
+		(condition-who      C)))
+    => #t #t #f 'ciao)
+
+  #f)
+
+
+(parameterize ((check-test-name		'&message))
+
+  (check
+      (let ((C	(make-message-condition "hello world")))
+	(values (condition?         C)
+		(message-condition? C)
+		(who-condition?     C)
+		(condition-message  C)))
+    => #t #t #f "hello world")
+
+  #f)
+
+
+(parameterize ((check-test-name		'&irritants))
+
+  (check
+      (let ((C	(make-irritants-condition '(1 2 3))))
+	(values (condition?         C)
+		(irritants-condition? C)
+		(who-condition?     C)
+		(condition-irritants  C)))
+    => #t #t #f '(1 2 3))
+
+  #f)
+
+
+(parameterize ((check-test-name		'&warning))
+
+  (check
+      (let ((C	(make-warning)))
+	(values (condition?         C)
+		(warning?           C)
+		(message-condition? C)))
+    => #t #t #f)
+
+  #f)
+
+
+(parameterize ((check-test-name		'&serious))
+
+  (check
+      (let ((C	(make-serious-condition)))
+	(values (condition?         C)
+		(serious-condition? C)
+		(message-condition? C)))
+    => #t #t #f)
+
+  #f)
+
+
+(parameterize ((check-test-name		'&error))
+
+  (check
+      (let ((C	(make-error)))
+	(values (condition?         C)
+		(serious-condition? C)
+		(error?             C)
+		(message-condition? C)))
+    => #t #t #t #f)
+
+  #f)
+
+
+(parameterize ((check-test-name		'&violation))
+
+  (check
+      (let ((C	(make-violation)))
+	(values (condition?         C)
+		(serious-condition? C)
+		(violation?         C)
+		(message-condition? C)))
+    => #t #t #t #f)
+
+  #f)
+
+
+(parameterize ((check-test-name		'&assertion))
+
+  (check
+      (let ((C	(make-assertion-violation)))
+	(values (condition?		C)
+		(serious-condition?	C)
+		(assertion-violation?	C)
+		(message-condition?	C)))
+    => #t #t #t #f)
+
+  #f)
+
+
+(parameterize ((check-test-name		'&non-continuable))
+
+  (check
+      (let ((C	(make-non-continuable-violation)))
+	(values (condition?			C)
+		(serious-condition?		C)
+		(non-continuable-violation?	C)
+		(message-condition?		C)))
+    => #t #t #t #f)
+
+  #f)
+
+
+(parameterize ((check-test-name		'&implementation-restriction))
+
+  (check
+      (let ((C	(make-implementation-restriction-violation)))
+	(values (condition?		C)
+		(serious-condition?	C)
+		(implementation-restriction-violation?	C)
+		(message-condition?	C)))
+    => #t #t #t #f)
+
+  #f)
+
+
+(parameterize ((check-test-name		'&lexical))
+
+  (check
+      (let ((C	(make-lexical-violation)))
+	(values (condition?		C)
+		(serious-condition?	C)
+		(lexical-violation?	C)
+		(message-condition?	C)))
+    => #t #t #t #f)
+
+  #f)
+
+
+(parameterize ((check-test-name		'&syntax))
+
+  (check
+      (let ((C	(make-syntax-violation)))
+	(values (condition?		C)
+		(serious-condition?	C)
+		(syntax-violation?	C)
+		(message-condition?	C)))
+    => #t #t #t #f)
+
+  #f)
+
+
+(parameterize ((check-test-name		'&undefined))
+
+  (check
+      (let ((C	(make-undefined-violation)))
+	(values (condition?		C)
+		(serious-condition?	C)
+		(undefined-violation?	C)
+		(message-condition?	C)))
+    => #t #t #t #f)
+
+  #f)
+
+
+(parameterize ((check-test-name		'compound))
+
+  (check
+      (let ((C	(condition (make-who-condition 'ciao)
+			   (make-message-condition "hello world")
+			   (make-irritants-condition '(1 2 3)))))
+	(values (condition?		C)
+		(serious-condition?	C)
+		(who-condition?		C)
+		(message-condition?	C)
+		(irritants-condition?	C)))
+    => #t #f #t #t #t)
+
+  (check
+      (let ((C	(condition (make-who-condition 'ciao)
+			   (condition (make-message-condition "hello world")
+				      (make-irritants-condition '(1 2 3))))))
+	(values (condition?		C)
+		(serious-condition?	C)
+		(who-condition?		C)
+		(message-condition?	C)
+		(irritants-condition?	C)))
+    => #t #f #t #t #t)
+
+  (check
+      (let ((C	(condition (condition (make-who-condition 'ciao)
+				      (make-message-condition "hello world")
+				      (make-irritants-condition '(1 2 3))))))
+	(values (condition?		C)
+		(serious-condition?	C)
+		(who-condition?		C)
+		(message-condition?	C)
+		(irritants-condition?	C)))
+    => #t #f #t #t #t)
+
+  (check
+      (let ((C	(condition)))
+	(values (condition?		C)
+		(serious-condition?	C)
+		(who-condition?		C)
+		(message-condition?	C)
+		(irritants-condition?	C)))
+    => #t #f #f #f #f)
+
+;;; --------------------------------------------------------------------
+
+  (check
+      (let* ((C	(condition))
+	     (S	(simple-conditions C)))
+	(values (length S)
+		(condition? (car S))
+		(serious-condition? (car S))))
+    => 1 #t #f)
+
+  (check
+      (let* ((C	(condition (make-who-condition 'ciao)
+			   (make-message-condition "hello world")
+			   (make-irritants-condition '(1 2 3))))
+	     (S	(simple-conditions C)))
+	#;(debug-print S)
+	(values (length			S)
+		(condition?		C)
+		(who-condition?		(list-ref S 0))
+		(message-condition?	(list-ref S 2))
+		(irritants-condition?	(list-ref S 4))))
+    => 6 #t #t #t #t)
+
+  (check
+      (let* ((C	(condition (make-who-condition 'ciao)
+			   (condition (make-message-condition "hello world")
+				      (make-irritants-condition '(1 2 3)))))
+	     (S	(simple-conditions C)))
+	(values (length			S)
+		(condition?		C)
+		(who-condition?		(list-ref S 0))
+		(message-condition?	(list-ref S 2))
+		(irritants-condition?	(list-ref S 4))))
+    => 6 #t #t #t #t)
+
+  (check
+      (let* ((C	(condition (condition (make-who-condition 'ciao)
+				      (make-message-condition "hello world"))
+			   (make-irritants-condition '(1 2 3))))
+	     (S	(simple-conditions C)))
+	(values (length			S)
+		(condition?		C)
+		(who-condition?		(list-ref S 0))
+		(message-condition?	(list-ref S 2))
+		(irritants-condition?	(list-ref S 4))))
+    => 6 #t #t #t #t)
+
+  #f)
+
+
+(parameterize ((check-test-name		'docs))
+
+  (let ()
+    (define-condition-type &c
+	&condition
+      make-c c?
+      (x c-x))
+
+    (define-condition-type &c1
+	&c
+      make-c1 c1?
+      (a c1-a))
+
+    (define-condition-type &c2
+	&c
+      make-c2 c2?
+      (b c2-b))
+
+    (define v1 (make-c1 "V1" "a1"))
+
+    (check (c? v1)        => #t)
+    (check (c1? v1)       => #t)
+    (check (c2? v1)       => #f)
+    (check (c-x v1)       => "V1")
+    (check (c1-a v1)      => "a1")
+
+
+
+    (define v2 (make-c2 "V2" "b2"))
+
+    (check (c? v2)        => #t)
+    (check (c1? v2)       => #f)
+    (check (c2? v2)       => #t)
+    (check (c-x v2)       => "V2")
+    (check (c2-b v2)      => "b2")
+
+
+
+    (define v3 (condition
+		 (make-c1 "V3/1" "a3")
+		 (make-c2 "V3/2" "b3")))
+
+    (check (c? v3)        => #t)
+    (check (c1? v3)       => #t)
+    (check (c2? v3)       => #t)
+    (check (c-x v3)       => "V3/1")
+    (check (c1-a v3)      => "a3")
+    (check (c2-b v3)      => "b3")
+
+
+
+    (define v4 (condition v1 v2))
+
+    (check (c? v4)        => #t)
+    (check (c1? v4)       => #t)
+    (check (c2? v4)       => #t)
+    (check (c-x v4)       => "V1")
+    (check (c1-a v4)      => "a1")
+    (check (c2-b v4)      => "b2")
+
+
+
+    (define v5 (condition v2 v3))
+
+    (check (c? v5)        => #t)
+    (check (c1? v5)       => #t)
+    (check (c2? v5)       => #t)
+    (check (c-x v5)       => "V2")
+    (check (c1-a v5)      => "a3")
+    (check (c2-b v5)      => "b2")
+
+    #f)
+
+  #f)
+
+
+(parameterize ((check-test-name		'fields))
+
+  ;;Test splitting arguments for constructors.
+  (let ()
+    (define-condition-type &alpha
+	&condition
+      make-alpha
+      alpha?
+      (a	alpha-a))
+
+    (define-condition-type &beta
+	&alpha
+      make-beta
+      beta?
+      (b1	beta-b1)
+      (b2	beta-b2))
+
+    (define-condition-type &delta
+	&beta
+      make-delta
+      delta?
+      (d1	delta-d1)
+      (d2	delta-d2)
+      (d3	delta-d3))
+
+    (define-condition-type &gamma
+	&delta
+      make-gamma
+      gamma?
+      (g1	gamma-g1)
+      (g2	gamma-g2)
+      (g3	gamma-g3)
+      (g4	gamma-g4))
+
+    (define A
+      (make-alpha 1))
+
+    (define B
+      (make-beta 1
+		 2 3))
+
+    (define D
+      (make-delta 1
+		  2 3
+		  4 5 6))
+
+    (define G
+      (make-gamma 1
+		  2 3
+		  4 5 6
+		  7 8 9 0))
+
+    (check
+	(alpha-a	A)
+      => 1)
+
+    (check
+	(values (alpha-a	B)
+		(beta-b1	B)
+		(beta-b2	B))
+      => 1 2 3)
+
+    (check
+	(values (alpha-a	D)
+		(beta-b1	D)
+		(beta-b2	D)
+		(delta-d1	D)
+		(delta-d2	D)
+		(delta-d3	D))
+      => 1 2 3 4 5 6)
+
+    (check
+	(values (alpha-a	G)
+		(beta-b1	G)
+		(beta-b2	G)
+		(delta-d1	G)
+		(delta-d2	G)
+		(delta-d3	G)
+		(gamma-g1	G)
+		(gamma-g2	G)
+		(gamma-g3	G)
+		(gamma-g4	G))
+      => 1 2 3 4 5 6 7 8 9 0)
+
+    #f)
+
+  #f)
 
 
 ;;;; done
